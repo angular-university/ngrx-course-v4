f6fb277601d43f0196ec6b283f5231d591b2eec2
JHades <jhades.dev@gmail.com>
2016-11-30 14:37:51 +0100

    ngrx course

---
+++ src/server/message.ts
@@ -0,0 +1,6 @@
+
+
+export interface Message  {
+    text:string;
+    read:boolean;
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,48 @@
 
+import * as _ from 'lodash';
 
-console.log('Server running ...');
+import * as faker from 'faker';
+import {Message} from "./message";
+import * as express from "express";
+import {Application} from 'express';
+const bodyParser = require('body-parser');
+
+const low = require('lowdb');
+
+const db = low();
+
+db.defaults({ messages: [] })
+    .value();
+
+
+
+_.times(100, () => {
+
+    const message: Message = {
+        text: faker.lorem.sentences(),
+        read: false
+    };
+
+    db.get('messages')
+        .push(message)
+        .value();
+});
+
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+
+
+app.route('/messages').get((req, res) => {
+
+    const messages = db.get("messages").value();
+
+    res.status(200).json({payload:messages});
+
+});
+
+
+app.listen(8090, () => {
+    console.log('Server is now running on port 8090 ...');
+});
