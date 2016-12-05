0d7c94d366edfdd2d4dedeeb21b8d959d35865cd
JHades <jhades.dev@gmail.com>
2016-12-05 10:41:15 +0100

    ngrx course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-import { MultiActorComponent } from './multi-actor/multi-actor.component';
-import { FormUndoComponent } from './form-undo/form-undo.component';
-import { MasterDetailComponent } from './master-detail/master-detail.component';
-import {RouterModule} from "@angular/router";
-import {routerConfig} from "../router.config";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MultiActorComponent,
-    FormUndoComponent,
-    MasterDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    RouterModule.forRoot(routerConfig)
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
