f794c3d4b6ea09b58e4f206ec0c153a2548d54dc
JHades <jhades.dev@gmail.com>
2016-12-05 23:51:33 +0100

    ngrx course

--- src/server/api/apiGetAllThreads.ts
+++ src/server/api/apiGetAllThreads.ts
@@ -1,11 +1,9 @@
-
-
 import {Application} from 'express';
 import {Thread} from "../model/thread";
 import {ThreadsVM} from "../view-model/threads.vm";
 import * as _ from 'lodash';
 import {dbThreads} from "../db/db-data";
-
+import {ThreadSummaryVM} from "../view-model/thread-summary.vm";
 
 
 export function apiGetAllThreads(app: Application) {
@@ -16,13 +14,24 @@ export function apiGetAllThreads(app: Application) {
 
         const threadsVm: ThreadsVM = {
             unreadThreadsCounter: 2,
-            threadSummaries: <any>threads
+            threadSummaries: <any>threads.map(mapThreadToThreadSummary)
         };
 
-        res.status(200).json({payload:threadsVm});
+        res.status(200).json({payload: threadsVm});
 
     });
 
 }
 
 
+function mapThreadToThreadSummary(thread: Thread): ThreadSummaryVM {
+
+
+    return {
+        participantNames: '',
+        timestamp: 0,
+        lastMessage: ""
+    };
+
+
+}
