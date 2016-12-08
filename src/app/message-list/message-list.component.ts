6a3fbc002429cdcd79d857e01a05b71773ee317c
JHades <jhades.dev@gmail.com>
2016-12-08 16:59:44 +0100

    ngrx course

--- src/app/message-list/message-list.component.ts
+++ src/app/message-list/message-list.component.ts
@@ -1,26 +1,50 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {
+    Component, OnInit, Input, ViewChildren, AfterViewChecked, QueryList, ElementRef,
+    OnChanges, ViewChild, Query, AfterViewInit
+} from '@angular/core';
 import {MessageVM} from "../../server/view-model/message.vm";
+import * as _ from 'lodash';
 
 @Component({
-  selector: 'message-list',
-  templateUrl: './message-list.component.html',
-  styleUrls: ['./message-list.component.css']
+    selector: 'message-list',
+    templateUrl: './message-list.component.html',
+    styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnInit, OnChanges {
 
-  @Input()
-  messages: MessageVM[];
+    @Input()
+    messages: MessageVM[];
 
-
-  constructor() {
-
-
-  }
+    @ViewChild('list')
+    list: ElementRef;
 
 
-  ngOnInit() {
+    constructor() {
 
-  }
+    }
+
+
+
+    ngOnChanges() {
+
+        setTimeout(() => {
+            if (this.list) {
+                const items =  this.list.nativeElement.querySelectorAll('li');
+                const lastItem = _.last(items);
+                console.log(lastItem);
+            }
+        });
+
+
+    }
+
+
+
+
+
+    ngOnInit() {
+
+    }
 
 
 }
