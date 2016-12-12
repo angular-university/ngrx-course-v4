fae1b6273d52ec4966e81911c319c087e5ee2947
JHades <jhades.dev@gmail.com>
2016-12-12 17:45:56 +0100

    ngrx course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,9 @@ import {MessageSectionComponent} from './message-section/message-section.compone
 import {ThreadListComponent} from './thread-list/thread-list.component';
 import {MessageListComponent} from './message-list/message-list.component';
 import {UserSelectionComponent} from './user-selection/user-selection.component';
-import {StoreModule, ActionReducer, combineReducers} from "@ngrx/store";
-import {ApplicationState, INITIAL_APPLICATION_STATE} from "./store/application-state";
-import {GET_USER_INFO_ACTION, LOAD_USER_THREADS_ACTION, SELECT_THREAD_ACTION} from "./store/actions";
-import {Participant} from "../shared/model/participant";
+import {StoreModule, combineReducers} from "@ngrx/store";
 import {ParticipantsService} from "./services/participants.service";
 import {ThreadsService} from "./services/threads.service";
-import {Thread} from "../shared/model/thread";
-import {AllUserData} from "../shared/model/all-user-data";
 import {uiState} from "./store/reducers/uiState";
 import {storeData} from "./store/reducers/storeData";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
