05c56c1dcbc072c5acff3d4beae0064c90a6ee2c
JHades <jhades.dev@gmail.com>
2016-12-12 17:45:35 +0100

    ngrx course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {HttpModule, RequestOptions} from '@angular/http';
-import { AppComponent } from './app.component';
-import { ThreadSectionComponent } from './thread-section/thread-section.component';
-import { MessageSectionComponent } from './message-section/message-section.component';
-import { ThreadListComponent } from './thread-list/thread-list.component';
-import { MessageListComponent } from './message-list/message-list.component';
-import { UserSelectionComponent } from './user-selection/user-selection.component';
+import {AppComponent} from './app.component';
+import {ThreadSectionComponent} from './thread-section/thread-section.component';
+import {MessageSectionComponent} from './message-section/message-section.component';
+import {ThreadListComponent} from './thread-list/thread-list.component';
+import {MessageListComponent} from './message-list/message-list.component';
+import {UserSelectionComponent} from './user-selection/user-selection.component';
 import {StoreModule, ActionReducer, combineReducers} from "@ngrx/store";
 import {ApplicationState, INITIAL_APPLICATION_STATE} from "./store/application-state";
 import {GET_USER_INFO_ACTION, LOAD_USER_THREADS_ACTION, SELECT_THREAD_ACTION} from "./store/actions";
@@ -18,30 +18,27 @@ import {Thread} from "../shared/model/thread";
 import {AllUserData} from "../shared/model/all-user-data";
 import {uiState} from "./store/reducers/uiState";
 import {storeData} from "./store/reducers/storeData";
-
-
-
-
-
-
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ThreadSectionComponent,
-    MessageSectionComponent,
-    ThreadListComponent,
-    MessageListComponent,
-    UserSelectionComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-     StoreModule.provideStore(combineReducers({uiState, storeData}))
-  ],
-  providers: [ParticipantsService, ThreadsService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ThreadSectionComponent,
+        MessageSectionComponent,
+        ThreadListComponent,
+        MessageListComponent,
+        UserSelectionComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        StoreModule.provideStore(combineReducers({uiState, storeData})),
+        StoreDevtoolsModule.instrumentOnlyWithExtension()
+    ],
+    providers: [ParticipantsService, ThreadsService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
