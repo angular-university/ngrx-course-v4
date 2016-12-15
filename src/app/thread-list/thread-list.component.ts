2f539c2265fbbfa3846d38a366519ee1cc318803
Your Name <you@example.com>
2016-12-15 11:55:47 +0100

    ngrx course

--- src/app/thread-list/thread-list.component.ts
+++ src/app/thread-list/thread-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-thread-list',
+  selector: 'thread-list',
   templateUrl: './thread-list.component.html',
   styleUrls: ['./thread-list.component.css']
 })
