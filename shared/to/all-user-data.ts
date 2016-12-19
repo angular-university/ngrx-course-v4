9633bc11e6fb7399b320165586df41fd6f0cef83
Your Name <you@example.com>
2016-12-19 13:00:03 +0100

    ngrx course

--- shared/to/all-user-data.ts
+++ shared/to/all-user-data.ts
@@ -5,7 +5,9 @@ import {Thread} from "../model/thread";
 import {Message} from "../model/message";
 
 export interface AllUserData {
+
     participants: Participant[];
     threads: Thread[];
     messages: Message[];
+
 }
