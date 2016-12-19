453b8a1902b778205c03b1ae401f54a5126aa3d5
Your Name <you@example.com>
2016-12-19 14:24:24 +0100

    ngrx course

--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -16,7 +16,7 @@ export class ThreadSectionComponent implements OnInit {
                 private store: Store<ApplicationState>) {
 
       store.subscribe(
-          console.log
+          state => console.log('thread section received state', state)
       );
 
   }
