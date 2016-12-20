a7d6bc236d916cb741a003bf90c1af9e9283b902
Your Name <you@example.com>
2016-12-20 12:21:19 +0100

    ngrx course

--- src/server/db-data.ts
+++ src/server/db-data.ts
@@ -93,8 +93,13 @@ export const dbMessages: {[key: number]: Message} = {
         text: 'OK, no problem',
         timestamp: new Date().getTime()
     },
-
-
+    5: {
+        id: 5,
+        threadId: 1,
+        participantId: 1,
+        text: 'Great',
+        timestamp: new Date().getTime()
+    },
     6: {
         id: 6,
         threadId: 2,
