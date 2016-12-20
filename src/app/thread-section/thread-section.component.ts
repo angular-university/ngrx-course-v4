365b1dc31bcb59bd4b7217186a06285ab6371d38
Your Name <you@example.com>
2016-12-20 12:29:22 +0100

    ngrx course

--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -43,12 +43,14 @@ export class ThreadSectionComponent implements OnInit {
                     const names =  _.keys(thread.participants).map(
                         participantId => state.storeData.participants[participantId].name );
 
-                    const lastMessageId = _.last(thread.messageIds);
+                    const lastMessageId = _.last(thread.messageIds),
+                           lastMessage = state.storeData.messages[lastMessageId];
 
                     return {
                         id: thread.id,
                         participantNames: _.join(names, ","),
-                        lastMessageText: state.storeData.messages[lastMessageId].text
+                        lastMessageText: lastMessage.text,
+                        timestamp: lastMessage.timestamp
                     }
 
                 });
--- src/app/thread-section/thread-summary.vm.ts
+++ src/app/thread-section/thread-summary.vm.ts
@@ -1,12 +1,12 @@
 
 
-import {Thread} from "../../../shared/model/thread";
 
 export interface ThreadSummaryVM {
 
     id:number;
     participantNames:string;
     lastMessageText:string;
+    timestamp:number;
 
 
 }
