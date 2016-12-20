da443d950fa8f220f45d46fd809441ce2b81d089
Your Name <you@example.com>
2016-12-20 14:22:38 +0100

    ngrx course

--- src/app/store/effects/load-threads-effect.service.ts
+++ src/app/store/effects/load-threads-effect.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {ThreadsService} from "../../services/threads.service";
 import {Actions, Effect} from "@ngrx/effects";
 import {LOAD_USER_THREADS_ACTION, UserThreadsLoadedAction} from "../actions";
+import {Observable} from "rxjs";
+import {Action} from "@ngrx/store";
+
 
 
 
@@ -13,7 +16,7 @@ export class LoadThreadsEffectService {
   }
 
 
-  @Effect() userThreads$ = this.actions$
+  @Effect() userThreads$: Observable<Action> = this.actions$
       .ofType(LOAD_USER_THREADS_ACTION)
       .switchMap(() => this.threadsService.loadUserThreads())
       .map(allUserData => new UserThreadsLoadedAction(allUserData) );
