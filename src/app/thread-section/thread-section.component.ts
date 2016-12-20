b40115dc2437015e9e712746e76f65c25e09fd71
Your Name <you@example.com>
2016-12-20 10:46:04 +0100

    ngrx course

--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -1,35 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ThreadsService} from "../services/threads.service";
 import {Store} from "@ngrx/store";
 import {ApplicationState} from "../store/application-state";
 import {LoadUserThreadsAction} from "../store/actions";
 import {Observable} from "rxjs";
-
-
+import * as _ from 'lodash';
+import {Thread} from "../../../shared/model/thread";
 
 @Component({
-  selector: 'thread-section',
-  templateUrl: './thread-section.component.html',
-  styleUrls: ['./thread-section.component.css']
+    selector: 'thread-section',
+    templateUrl: './thread-section.component.html',
+    styleUrls: ['./thread-section.component.css']
 })
 export class ThreadSectionComponent implements OnInit {
 
-  userName$: Observable<string>;
+    userName$: Observable<string>;
+    unreadMessagesCounter$: Observable<number>;
 
-  constructor(private threadsService: ThreadsService,
+    constructor(private threadsService: ThreadsService,
                 private store: Store<ApplicationState>) {
 
-      this.userName$ = store
-                          .skip(1)
-                          .map(this.mapStateToUserName);
+        this.userName$ = store
+            .skip(1)
+            .map(this.mapStateToUserName);
 
-  }
+        this.unreadMessagesCounter$ = store.skip(1)
+            .map(this.mapStateToUnreadMessagesCounter);
 
-  mapStateToUserName(state:ApplicationState):string {
-      return state.storeData.participants[state.uiState.userId].name;
-  }
+    }
 
-  ngOnInit() {
+    mapStateToUserName(state: ApplicationState): string {
+        return state.storeData.participants[state.uiState.userId].name;
+    }
+
+
+    mapStateToUnreadMessagesCounter(state: ApplicationState): number {
+
+        const currentUserId = state.uiState.currentThreadId;
+
+        return _.values<Thread>(state.storeData.threads)
+            .reduce(
+                (acc, thread) => acc + thread.participants[currentUserId]
+
+                ,0);
+    }
+
+    ngOnInit() {
 
         this.threadsService.loadUserThreads()
             .subscribe(
@@ -38,7 +54,7 @@ export class ThreadSectionComponent implements OnInit {
                 )
             );
 
-  }
+    }
 
 }
 
