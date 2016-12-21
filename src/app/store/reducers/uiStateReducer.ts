4f7f51ecd714f73212c9085306c0a8ca5bcc5dd5
Your Name <you@example.com>
2016-12-21 13:18:27 +0100

    ngrx course

--- src/app/store/reducers/uiStateReducer.ts
+++ src/app/store/reducers/uiStateReducer.ts
@@ -3,10 +3,21 @@
 
 import {UiState, INITIAL_UI_STATE} from "../ui-state";
 import {Action} from "@ngrx/store";
+import {THREAD_SELECTED_ACTION} from "../actions";
+
 
 export function uiState(state: UiState = INITIAL_UI_STATE, action: Action) : UiState {
 
     switch (action.type)  {
+
+        case THREAD_SELECTED_ACTION:
+
+            const newState = Object.assign({}, state);
+
+            newState.currentThreadId = action.payload;
+
+            return newState;
+
 
         default:
             return state;
