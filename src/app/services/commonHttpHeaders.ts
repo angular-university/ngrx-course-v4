433a60223aba4c6082e3fa698f26f787ce1e3301
Your Name <you@example.com>
2017-01-09 14:28:59 +0100

    ngrx course

--- src/app/services/commonHttpHeaders.ts
+++ src/app/services/commonHttpHeaders.ts
@@ -5,5 +5,6 @@ import {Headers} from "@angular/http";
 export function commonHttpHeaders(userId: number) {
     const headers = new Headers();
     headers.append('USERID',userId.toString());
+    headers.append('Content-Type', 'application/json; charset=utf-8');
     return {headers};
 }
