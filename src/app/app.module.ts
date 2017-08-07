3874890db61e03ad4b6237935b6c43fa8f750041
JHades <jhades.dev@gmail.com>
2017-08-07 15:34:46 +0200

    fix issue "Calling function 'combineReducers', function calls are not supported"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,8 @@ import { MessageSectionComponent } from './message-section/message-section.compo
 import { ThreadListComponent } from './thread-list/thread-list.component';
 import { MessageListComponent } from './message-list/message-list.component';
 import {ThreadsService} from "./services/threads.service";
-import {StoreModule, combineReducers} from "@ngrx/store";
-import {INITIAL_APPLICATION_STATE} from "./store/application-state";
+import {StoreModule, combineReducers, Action} from "@ngrx/store";
+import {ApplicationState, INITIAL_APPLICATION_STATE} from "./store/application-state";
 import {EffectsModule} from "@ngrx/effects";
 import {LoadThreadsEffectService} from "./store/effects/load-threads-effect.service";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
@@ -19,6 +19,18 @@ import {uiState} from "./store/reducers/uiStateReducer";
 import {storeData} from "./store/reducers/uiStoreDataReducer";
 import {WriteNewMessageEffectService} from "./store/effects/write-new-message-effect.service";
 import {ServerNotificationsEffectService} from "./store/effects/server-notifications-effect.service";
+
+
+const reducers = {
+    uiState,
+    storeData
+};
+
+const combinedReducer = combineReducers(reducers);
+
+export function storeReducer(state: ApplicationState, action: Action) {
+    return combinedReducer(state, action);
+}
 
 
 @NgModule({
@@ -34,7 +46,7 @@ import {ServerNotificationsEffectService} from "./store/effects/server-notificat
     BrowserModule,
     FormsModule,
     HttpModule,
-      StoreModule.provideStore(combineReducers({uiState,storeData}), INITIAL_APPLICATION_STATE),
+      StoreModule.provideStore(storeReducer, INITIAL_APPLICATION_STATE),
       EffectsModule.run(LoadThreadsEffectService),
       EffectsModule.run(WriteNewMessageEffectService),
       EffectsModule.run(ServerNotificationsEffectService),
