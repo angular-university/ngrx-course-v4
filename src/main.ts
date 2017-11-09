0a96b414fb55378af7522c3cdf9c0cdc48ac46b4
JHades <jhades.dev@gmail.com>
2017-11-09 14:27:09 +0100

    upgrade to Angular 5 and Ngrx 4

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,48 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import {Observable} from "rxjs/Observable";
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/skip';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
+
+import "rxjs/add/observable/interval";
+import "rxjs/add/observable/of";
+
+const debuggerOn = true;
+
+import 'rxjs/add/operator/do';
+
+declare module 'rxjs/Observable' {
+    interface Observable<T> {
+        debug: (...any) => Observable<T>
+    }
+}
+
+Observable.prototype.debug = function(message:string) {
+    return this.do(
+        nextValue => {
+            if (debuggerOn) {
+                console.log(message, nextValue)
+            }
+        },
+        error => {
+            if (debuggerOn) {
+                console.error(message, error)
+            }
+        },
+        () => {
+            if (debuggerOn) {
+                console.log("Observable completed - ", message)
+            }
+        }
+    );
+};
+
 
 if (environment.production) {
   enableProdMode();
